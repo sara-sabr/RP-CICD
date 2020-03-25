cd448f3cd6130875e80dc3feefca29f58c821f56
Eric Wu <e-wu@users.noreply.github.com>
2020-03-25 11:36:38 -0400

    Fix permission and naming

--- pipeline-tools/sonarqube/install.sh
+++ pipeline-tools/sonarqube/install.sh
@@ -9,9 +9,11 @@ sysctl -w vm.max_map_count=262144
 sysctl -w fs.file-max=65536
 
 # Move to server location
-mkdir -p /srv/sonarcube/data /srv/sonarcube/logs /srv/sonarcube/extensions
-cp docker-compose.yml /srv/sonarcube/docker-compose.yml
+mkdir -p /srv/sonarqube/data /srv/sonarqube/logs /srv/sonarqube/extensions
+cp docker-compose.yml /srv/sonarqube/docker-compose.yml
+chown 999:999 /srv/sonarqube/data /srv/sonarqube/logs /srv/sonarqube/extensions
+chmod 770 /srv/sonarqube/data /srv/sonarqube/logs /srv/sonarqube/extensions
 
 # Install the service script
-cp sonarcube.service /etc/systemd/system/sonarcube.service
-systemctl enable sonarcube
+cp sonarqube.service /etc/systemd/system/sonarqube.service
+systemctl enable sonarqube
